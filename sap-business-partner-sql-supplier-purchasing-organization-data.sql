489ba20b6d4e784df8563dce5145878d327cb4bf
Yurino Oga <yurino.o@latona.io>
2021-11-04 18:12:32 +0900

    release

--- sap-business-partner-sql-supplier-purchasing-organization-data.sql
+++ sap-business-partner-sql-supplier-purchasing-organization-data.sql
@@ -15,6 +15,6 @@ CREATE TABLE `sap-business-partner-supplier-purchasing-organization-data`
     `SupplierRespSalesPersonName` varchar(30) DEFAULT NULL,
     `DeletionIndicator`     varchar(1) DEFAULT NULL,
     PRIMARY KEY (`Supplier`, `PurchasingOrganization`),
-    CONSTRAINT `Supplier_fk` FOREIGN KEY (`Supplier`) REFERENCES `sap-business-partner-role-data` (`ManufacturingOrder`)
+    CONSTRAINT `Supplier_fk` FOREIGN KEY (`Supplier`) REFERENCES `sap-business-partner-role-data` (`BusinessPartner`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
