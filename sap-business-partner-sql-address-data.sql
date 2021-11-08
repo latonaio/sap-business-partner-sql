ba70c6c6c1ee287c0b5008ae39831171647e3ed0
Yurino Oga <yurino.o@latona.io>
2021-11-09 08:48:27 +0900

    release

---
+++ sap-business-partner-sql-address-data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `sap-business-partner-address-data`
+(
+    `BusinessPartner`       varchar(10) DEFAULT NULL,
+    `AddressID`             varchar(10) DEFAULT NULL,
+    `ValidityStartDate`     date DEFAULT NULL,
+    `ValidityEndDate`       date DEFAULT NULL,
+    `Country`               varchar(3) DEFAULT NULL,
+    `Region`                varchar(3) DEFAULT NULL,
+    `StreetName`            varchar(60) DEFAULT NULL,
+    `CityName`              varchar(40) DEFAULT NULL,
+    `PostalCode`            varchar(10) DEFAULT NULL,
+    `Language`              varchar(2) DEFAULT NULL,
+    `to_EmailAddress`       varchar(241) DEFAULT NULL,
+    `to_FaxNumber`          varchar(30) DEFAULT NULL,
+    `to_MobilePhoneNumber`  varchar(30) DEFAULT NULL,
+    `to_PhoneNumber`        varchar(30) DEFAULT NULL,
+    `to_URLAddress`         varchar(132) DEFAULT NULL,
+    PRIMARY KEY (`BusinessPartner`),
+    CONSTRAINT `BusinessPartner_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `sap-business-partner-role-data` (`BusinessPartner`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
