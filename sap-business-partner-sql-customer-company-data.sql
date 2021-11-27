eac2d9893a825bd0ecd3d69ccf720c29d5d4757e
Yurino Oga <yurino.o@latona.io>
2021-11-27 16:51:25 +0900

    release

--- sap-business-partner-sql-customer-company-data.sql
+++ sap-business-partner-sql-customer-company-data.sql
@@ -4,12 +4,12 @@ CREATE TABLE `sap-business-partner-customer-company-data`
     `Customer_desc`         varchar(40) DEFAULT NULL,
     `CompanyCode`           varchar(4) DEFAULT NULL,
     `APARToleranceGroup`    varchar(4) DEFAULT NULL,
-    `CustomerSupplierClearingIsUsed` varchar(1) DEFAULT NULL,
+    `CustomerSupplierClearingIsUsed` bool DEFAULT NULL,
     `HouseBank`             varchar(5) DEFAULT NULL,
     `PaymentMethodsList`    varchar(10) DEFAULT NULL,
     `PaymentTerms`          varchar(4) DEFAULT NULL,
     `ReconciliationAccount` varchar(10) DEFAULT NULL,
-    `DeletionIndicator`     varchar(1) DEFAULT NULL,
+    `DeletionIndicator`     bool DEFAULT NULL,
     PRIMARY KEY (`Customer`, `CompanyCode`),
     CONSTRAINT `Customer_fk` FOREIGN KEY (`Customer`) REFERENCES `sap-business-partner-role-data` (`BusinessPartner`)
 ) ENGINE = InnoDB
--- sap-business-partner-sql-role-data.sql
+++ sap-business-partner-sql-role-data.sql
@@ -2,8 +2,8 @@ CREATE TABLE `sap-business-partner-role-data`
 (
     `BusinessPartner`       varchar(10) DEFAULT NULL,
     `BusinessPartnerRole`   varchar(7) DEFAULT NULL,
-    `ValidFrom`             date DEFAULT NULL,
-    `ValidTo`               date DEFAULT NULL,
+    `ValidFrom`             varchar(8) DEFAULT NULL,
+    `ValidTo`               varchar(8) DEFAULT NULL,
     PRIMARY KEY (`BusinessPartner`, `BusinessPartnerRole`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
